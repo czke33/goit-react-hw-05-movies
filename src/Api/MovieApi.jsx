4f7a44e49a397ca54e9a160dacb0a9c74d79d965
czke33 <czke33@gmail.com>
2024-05-02 12:53:23 +0300

    add credit cast component

---
+++ src/Api/MovieApi.jsx
@@ -0,0 +1,57 @@
+
+import axios from 'axios';
+
+const API_KEY = '714c3120d8fef346bdc59740f67d43e6';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+export async function getHomeMovies() {
+    const response = await axios.get('/trending/movie/week', {
+        params: {
+            api_key: API_KEY,
+            language: 'en-US',
+            sort_by: 'popularity.desc',
+            page: 1
+        }
+    });
+    return response.data;
+}
+export async function getMovieByName(query, page) {
+    const response = await axios.get('/search/movie', {
+        params: {
+            api_key: API_KEY,
+            language: 'en-US',
+            query: query,
+            page: page
+        }
+    });
+    return response.data;
+}
+
+export async function getMovieDetalis(id) {
+    const response = await axios.get('/movie/' + id, {
+        params: {
+            api_key: API_KEY,
+            language: 'en-US'
+        }
+    });
+    return response.data;
+}
+export async function getMovieCredits(id) {
+    const response = await axios.get('/movie/' + id + '/credits', {
+        params: {
+            api_key: API_KEY,
+            language: 'en-US'
+        }
+    });
+    return response.data;
+}
+export async function getMovieReviews(id) {
+    const response = await axios.get('/movie/' + id + '/reviews', {
+        params: {
+            api_key: API_KEY,
+            language: 'en-US'
+        }
+    });
+    return response.data;
+}
---
+++ src/components/Credits/Credits.jsx
@@ -0,0 +1,59 @@
+import { getMovieCredits } from "Api/MovieApi";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import style from './Credits.module.css'
+
+const pageStatus = {
+    loading: 'loading',
+    success: 'success',
+    error: 'error',
+    init: 'init'
+};
+
+export const Credits = () => {
+    const [cast, setCast] = useState([]);
+    const [status, setStatus] = useState(pageStatus.init);
+
+    const params = useParams();
+    const movieId = params.id;
+    
+    useEffect(() => {
+        setStatus(pageStatus.loading);
+        async function fetchData() {
+            try {
+                const data = await getMovieCredits(movieId);
+                setCast(data.cast);
+                setStatus(pageStatus.success);
+            }
+            catch (error) {
+                setStatus(pageStatus.error);
+            }
+        }
+        fetchData();
+    }, [movieId]);
+
+    return (
+        <>
+        {status === pageStatus.loading && <p>Loading...</p>}
+        {status === pageStatus.error && <p className={style.error}>Error</p>}
+        {status === pageStatus.success && cast.lenght === 0 && <p className={style.error}>No cast found</p>}
+        {status === pageStatus.success && (
+            <ul className={style.photoList}>
+            {cast.map((item, index) => (
+                <li className={style.photoItem} key={index}>
+                    <img className={style.photo}
+                        src={item.profile_path
+                            ? `https://image.tmdb.org/t/p/w200${item.profile_path}`
+                            : ''} alt={item.name} />
+                    <p className={style.name}>{item.name}</p>
+                    <p className={style.role}>{item.character ? item.character : 'role'}</p>
+                </li>
+            ))}
+            </ul>
+        )}
+    </>
+    );
+
+};
+
+export default Credits;
