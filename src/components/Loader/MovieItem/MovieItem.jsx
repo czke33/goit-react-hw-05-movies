a4c722139b441b356d02e8c77d21b3b9bcd06749
czke33 <czke33@gmail.com>
2024-05-02 14:08:05 +0300

    add movieitem component

---
+++ src/components/Loader/MovieItem/MovieItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import style from './MovieItem.module.css';
+import PropType from 'prop-types'
+
+const MovieItem = ({ url, title, activeId, location }) => {
+    return (
+        <li className={style.item}>
+            <Link to={{`/movies/${activeId}` state={{ from: location }}>
+                <img className={style.img} src={url !== null ? `https://image.tmdb.org/t/p/w200${url}` : ''} alt={title} />
+                
+                <p className={style.label}>{title}</p>
+            </Link>
+        </li>
+    );
+};
+MovieItem.propType = {
+    url: PropType.string.isRequired,
+    title: PropType.string.isRequired,
+    activeId: PropType.number.isRequired,
+    location: PropType.object.isRequired
+};
+
+export default MovieItem;
